728739626845545099cefed7a3fd68d4f1b9bf47
blue9076r <nobfi517@gmail.com>
2025-01-27 20:14:27 -0600

    Staircase

--- maps/Tracey_house_in_2.lua
+++ maps/Tracey_house_in_2.lua
@@ -9,6 +9,9 @@ World.Map[3]:setLineH(1,5,4,Wall)
 World.Map[3]:setLineH(1,6,4,Wall)
 World.Map[3]:setLineH(1,7,4,Wall)
 
+World.Map[3]:setLineV(5,2,3,Stairs,AntiVoid)
+World.Map[3]:setLineV(5,4,5,Stairs)
+
 World.Map[3]:setTile(7,1,Brick_D)
 World.Map[3]:setTile(6,7,Carpet_V)
 
@@ -25,6 +28,7 @@ World.Map[3]:addWall(Box.new(0,0,8,14))
 World.Map[3]:addWall(Box.new(8,0,4,2))
 World.Map[3]:addWall(Box.new(12,0,2,1))
 World.Map[3]:addWall(Box.new(14,0,8,2))
+World.Map[3]:addWall(Box.new(10,2,0.1,8))
 
 World.Map[3]:addWall(Box.new(9.9,13.9,2.2,0.1,1,1,function()
 	print("Exit Living Room")
@@ -32,9 +36,10 @@ World.Map[3]:addWall(Box.new(9.9,13.9,2.2,0.1,1,1,function()
 	setPlr(10.5,0.5)
 end))
 
-World.Map[3]:addWall(Box.new(7.9,2,2.2,0.1,1,1,function()
+World.Map[3]:addWall(Box.new(8,6.5,1.8,0.1,1,1,function()
 	print("raise")
 	Map = World.Map[4]
+	setPlr(Plr.x,5.25)
 end))
 
 World.Map[3]:addWall(Box.new(11.9,1,2.2,0.1,1,1,function()
--- maps/Tracey_house_in_3.lua
+++ maps/Tracey_house_in_3.lua
@@ -5,11 +5,16 @@ World.Map[4]:setLineV(2,1,7,Void)
 World.Map[4]:setLineV(3,1,7,Void)
 World.Map[4]:setLineV(4,1,7,Void)
 World.Map[4]:setLineH(5,1,11,Void)
+World.Map[4]:setLineV(5,2,3,Stairs)
+World.Map[4]:setLineV(5,4,5,Stairs,AntiVoid)
 
 World.Map[4]:addWall(Box.new(0,0,8,14))
 World.Map[4]:addWall(Box.new(8,0,14,2))
+World.Map[4]:addWall(Box.new(10,4,0.1,6))
+World.Map[4]:addWall(Box.new(8,10,2,0.1))
 
-World.Map[4]:addWall(Box.new(7.9,13,2.2,0.1,1,1,function()
+World.Map[4]:addWall(Box.new(8,6.5,1.8,0.1,1,1,function()
 	print("lower")
 	Map = World.Map[3]
+	setPlr(Plr.x,7.75)
 end))
