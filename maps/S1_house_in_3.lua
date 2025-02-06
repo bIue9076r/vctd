5124fcc2b082009a579efbba93e8075348ad88c9
blue <nobfi517@gmail.com>
2025-02-05 21:56:58 -0600

    Neighboor's House

---
+++ maps/S1_house_in_3.lua
@@ -0,0 +1,20 @@
+World.Map[12] = MapObj.new(Planks)
+
+World.Map[12]:setLineV(1,1,7,Void)
+World.Map[12]:setLineV(2,1,7,Void)
+World.Map[12]:setLineV(3,1,7,Void)
+World.Map[12]:setLineV(4,1,7,Void)
+World.Map[12]:setLineH(5,1,11,Void)
+World.Map[12]:setTile(5,2,Planks)
+World.Map[12]:setTile(5,3,Stairs)
+World.Map[12]:setLineV(5,4,5,Stairs,AntiVoid)
+
+World.Map[12]:addWall(Box.new(0,0,8,14))
+World.Map[12]:addWall(Box.new(8,0,14,2))
+World.Map[12]:addWall(Box.new(10,4,0.1,6))
+World.Map[12]:addWall(Box.new(8,10,2,0.1))
+
+World.Map[12]:addWall(Box.new(8,6.5,1.8,0.1,1,1,function()
+	Map = World.Map[10]
+	setPlr(Plr.x,7.75)
+end))
