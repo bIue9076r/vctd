00b2a2263f510e0a1e721620ebd670d1b5a2dcd7
blue <nobfi517@gmail.com>
2025-02-07 23:21:59 -0600

    House 3/12

--- maps/Varisa_house_out_1.lua
+++ maps/Varisa_house_out_1.lua
@@ -42,7 +42,8 @@ World.Map[8]:addWall(Box.new(17.9,7,2.2,0.1,1,1,function()
 end))
 
 World.Map[8]:addWall(Box.new(0,8.5,0.1,5.5,1,1,function()
-	print("To Next House")
+	Map = World.Map[13]
+	setPlr(20.5,Plr.y)
 end))
 
 World.Map[8]:addWall(Box.new(21.9,8.5,0.1,5.5,1,1,function()
