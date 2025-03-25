6096fcbd5703da52f28280c1b662c1707bc91d0b
blue <nobfi517@gmail.com>
2025-03-25 05:10:14 -0500

    add cracked vases

--- maps/S1_house_in_1.lua
+++ maps/S1_house_in_1.lua
@@ -103,4 +103,8 @@ if House[1].HouseTrait == "Messy" then
 	World.Map[14]:addProp(Prop.new(13.0,5.5,Trash_P))
 	World.Map[14]:addProp(Prop.new(12.5,6.0,Trash_P))
 	World.Map[14]:addProp(Prop.new(13.0,6.0,Trash_P))
+	
+	World.Map[14]:addProp(Prop.new(8,12.5,Vase_C))
+	World.Map[14]:addProp(Prop.new(13,12.5,Vase_C))
+	World.Map[14]:addProp(Prop.new(8,1.5,Vase_C))
 end
