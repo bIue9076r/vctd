03ae4531394fecf2c856ba36e20fdec4bbe22258
blue <nobfi517@gmail.com>
2025-04-09 03:50:07 -0500

    3rd floor traits

--- maps/S1_house_in_3.lua
+++ maps/S1_house_in_3.lua
@@ -21,3 +21,15 @@ World.Map[17]:addWall(Box.new(8,6.5,1.8,0.1,1,1,function()
 	Map = World.Map[15]
 	setPlr(Plr.x,7.75)
 end))
+
+if House[1].HouseTrait == "Neat" then
+	
+end
+
+if House[1].HouseTrait == "Messy" then
+	
+end
+
+if House[1].HouseTrait == "Fanatic" then
+	
+end
