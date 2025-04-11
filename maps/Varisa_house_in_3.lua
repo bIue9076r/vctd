4601e6344c00f453b8a75a0e038d06269a1b79a0
blue <nobfi517@gmail.com>
2025-04-10 21:56:12 -0500

    Attic boxes place holder

--- maps/Varisa_house_in_3.lua
+++ maps/Varisa_house_in_3.lua
@@ -12,6 +12,21 @@ World.Map[12]:setLineV(6,3,5,Planks,Bar_L)
 World.Map[12]:setTile(6,6,Planks,Bar_TL)
 World.Map[12]:setTile(5,6,Planks,Bar_T)
 
+World.Map[12]:addProp(Prop.new(17,5,ERROBJ,1))
+World.Map[12]:addProp(Prop.new(18,5,ERROBJ,1))
+World.Map[12]:addProp(Prop.new(17,6,ERROBJ,1))
+World.Map[12]:addProp(Prop.new(18,6,ERROBJ,1))
+
+World.Map[12]:addProp(Prop.new(11,11,ERROBJ,1))
+World.Map[12]:addProp(Prop.new(12,11,ERROBJ,1))
+World.Map[12]:addProp(Prop.new(11,12,ERROBJ,1))
+World.Map[12]:addProp(Prop.new(12,12,ERROBJ,1))
+
+World.Map[12]:addProp(Prop.new(18,10,ERROBJ,1))
+World.Map[12]:addProp(Prop.new(19,10,ERROBJ,1))
+World.Map[12]:addProp(Prop.new(18,11,ERROBJ,1))
+World.Map[12]:addProp(Prop.new(19,11,ERROBJ,1))
+
 World.Map[12]:addWall(Box.new(0,0,8,14))
 World.Map[12]:addWall(Box.new(8,0,14,2))
 World.Map[12]:addWall(Box.new(10,4,0.1,6))
