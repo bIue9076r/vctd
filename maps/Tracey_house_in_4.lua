bd4c1d600c0dc446f4b8e029cd43a4d9b97dcc4e
blue <nobfi517@gmail.com>
2025-06-09 21:13:33 -0500

    Bed place holder

--- maps/Tracey_house_in_4.lua
+++ maps/Tracey_house_in_4.lua
@@ -13,6 +13,13 @@ World.Map[6]:addWall(Box.new(12,0,10,2))
 
 World.Map[6]:addProp(Prop.new(14.6,0.5,Poster_Bu))
 
+World.Map[6]:addProp(Prop.new(12,2,ERROBJ,1,1))
+World.Map[6]:addProp(Prop.new(12,2,ERROBJ,1,1))
+World.Map[6]:addProp(Prop.new(12,3,ERROBJ,1,1))
+World.Map[6]:addProp(Prop.new(12,3,ERROBJ,1,1))
+World.Map[6]:addProp(Prop.new(12,4,ERROBJ,1,1))
+World.Map[6]:addProp(Prop.new(12,4,ERROBJ,1,1))
+
 World.Map[6]:addWall(Box.new(21.9,2,0.1,1,1,1,function()
 	Map = World.Map[3]
 	setPlr(8.5,Plr.y)
