63f7a73bdd6622c28011af5f37c60b4a2352c9a9
blue <nobfi517@gmail.com>
2025-06-13 12:58:42 -0500

    Seed

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ require("/Modules/box")
 require("/Modules/ticker")
 require("/Modules/string")
 require("/Modules/spString")
-require("/Modules/house")
+--require("/Modules/house")
 require("/Locales/English")
 require("/Locales/French")
 require("/Modules/Dialoge")
@@ -363,11 +363,15 @@ function love.load()
 	love.window.setMode(600, 400, {resizable = true, minwidth = 600, minheight = 400})
 	love.window.setTitle(String.get(3))
 	
-	-- TODO: add a save feature
-	--Save = love.filesystem.getInfo("/Save/.SaveFile")
-	
 	muted = true
 	Play.scene = Play.Scenes[math.random(1,3)]
+	
+	-- TODO: add a save feature
+	--Save = love.filesystem.getInfo("/Save/.SaveFile")
+	Seed = math.random(0,0xFFFFF)
+	math.randomseed(Seed)
+	print(string.format("Seed: 0x%05X",Seed))
+	require("/Modules/house")
 	
 	local mods = love.filesystem.getInfo("/mods/")
 	if mods then
