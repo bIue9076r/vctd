35fc18c0e632580bc13f2d1249a72792fb6ac6ad
blue <nobfi517@gmail.com>
2025-08-07 12:29:57 -0500

    Save state visuals

--- States/Save.lua
+++ States/Save.lua
@@ -1,5 +1,15 @@
 function Save_Draw()
+	-- draw background
+	drawBackgound()
 	
+	love.graphics.draw(
+		image.getImage(CtS[Plr.c]),
+		((SCREEN_X*19)/96),
+		((SCREEN_Y*11)/32),
+		0,
+		(SCREEN_X/120),
+		(SCREEN_Y/80)
+	)
 end
 
 function Save_Update(dt)
