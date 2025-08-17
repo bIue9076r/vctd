fec4fe60a2da3f203f7894901d70a652007a8f58
blue <nobfi517@gmail.com>
2025-08-16 23:19:48 -0500

    French fix

--- dialogue/French/Dialogues.lua
+++ dialogue/French/Dialogues.lua
@@ -62,7 +62,7 @@ require("/dialogue/French/general/mailboxes")
 World.Dialogue[French][18] = Dialogue.new({
 	Text.new("Bonjour Varisa.",Wilkie),
 	Text.new("Salut.",Varisa),
-	Text.new("What brings you over here today?",Wilkie),
+	Text.new("Quoi t'amène ici aujourd'hui?",Wilkie),
 	Text.new("Mme. Fisher a dit que vous avez sa boîte à outils.",Varisa),
 	Text.new("Elle en a besoin?",Wilkie),
 	Text.new("Non non non.",Varisa),
@@ -118,7 +118,7 @@ World.Dialogue[French][23] = Dialogue.new({
 	Text.new("Salut Varisa.",Lassie),
 	Text.new("Salut.",Varisa),
 	Text.new("Sais-tu où est la boîte à outils?",Varisa),
-	Text.new("I think i gave it to Wilkie a few days ago...",Lassie),
+	Text.new("Je pense j'ai le donné à Wilkie il y a quelques jours...",Lassie),
 	Text.new("Oh.",Varisa),
 	Text.new("Ouais, desolé.",Lassie),
 	Text.new("Il devrait toujours l'avoir.",Lassie),
@@ -145,66 +145,66 @@ World.Dialogue[French][28] = Dialogue.new({
 },1)
 
 World.Dialogue[French][29] = Dialogue.new({
-	Text.new("Did you get the toolbox?",Rachel),
-	Text.new("Wilkie's still using it.",Varisa),
-	Text.new("But he'll let us borrow the tool you need.",Varisa),
-	Text.new("Okay.",Rachel),
-	Text.new("Ask him for the screwdriver.",Rachel),
-	Text.new("Or better yet, take my guitar.",Rachel,nil,function()
+	Text.new("As-tu obtenu la boîte à outils?",Rachel),
+	Text.new("Wilkie est en utiliser encore.",Varisa),
+	Text.new("Mais, il nous laissera emprunter l'outil tu as besoin.",Varisa),
+	Text.new("Ouais.",Rachel),
+	Text.new("Demande-lui le tournevis.",Rachel),
+	Text.new("Ou mieux encore, prends ma guitare.",Rachel,nil,function()
 		Story.set("Rachel_Guitar_1",true)
 	end),
-	Text.new("You're the one thats gonna fix it anyway.",Rachel),
-	Text.new("I'm counting on you.",Rachel),
+	Text.new("Maintenant tu peux le réparer.",Rachel),
+	Text.new("Je t'attends.",Rachel),
 },1)
 
 World.Dialogue[French][30] = Dialogue.new({
-	Text.new("Did you get the toolbox?",Rachel),
-	Text.new("No but our old math teacher gave me this.",Varisa),
-	Text.new("No way!",Rachel),
-	Text.new("That's exactly what I need!",Rachel),
-	Text.new("Well, that works I guess.",Varisa),
-	Text.new("Here, take my guitar.",Rachel,nil,function()
+	Text.new("As-tu obtenu la boîte à outils?",Rachel),
+	Text.new("Non mais, notre vieille professeur de math m'a donner ça.",Varisa),
+	Text.new("Mais non!",Rachel),
+	Text.new("C'est exactement quel j'ai besoins!",Rachel),
+	Text.new("Eh bien, ça marche je suppose.",Varisa),
+	Text.new("Voilà, prends ma guitare.",Rachel,nil,function()
 		Story.set("Rachel_Guitar_1",true)
 	end),
-	Text.new("Now you can fix it.",Rachel),
-	Text.new("I'm counting on you.",Rachel),
+	Text.new("Maintenant tu peux le réparer.",Rachel),
+	Text.new("Je t'attends.",Rachel),
 },1)
 
 World.Dialogue[French][31] = Dialogue.new({
-	Text.new("Did you get the toolbox?",Rachel),
-	Text.new("Wilkie's still using it.",Varisa),
-	Text.new("But he'll let us borrow the tool you need.",Varisa),
-	Text.new("Okay.",Rachel),
-	Text.new("Ask him for the screwdriver.",Rachel),
-	Text.new("Screwdriver?",Varisa),
-	Text.new("Yeah?",Varisa),
-	Text.new("Look what our old math teacher gave me.",Varisa),
-	Text.new("No way!",Rachel),
-	Text.new("That's exactly what I need!",Rachel),
-	Text.new("Well, that works I guess.",Varisa),
-	Text.new("Here, take my guitar.",Rachel,nil,function()
+	Text.new("As-tu obtenu la boîte à outils?",Rachel),
+	Text.new("Wilkie est en utiliser encore.",Varisa),
+	Text.new("Mais, il nous laissera emprunter l'outil tu as besoin.",Varisa),
+	Text.new("Ouais.",Rachel),
+	Text.new("Demande-lui le tournevis.",Rachel),
+	Text.new("Tournevis?",Varisa),
+	Text.new("Ouais?",Varisa),
+	Text.new("Voir quoi notre vieille professeur de math m'a donner.",Varisa),
+	Text.new("Mais non!",Rachel),
+	Text.new("C'est exactement quel j'ai besoins!",Rachel),
+	Text.new("Eh bien, ça marche je suppose.",Varisa),
+	Text.new("Voilà, prends ma guitare.",Rachel,nil,function()
 		Story.set("Rachel_Guitar_1",true)
 	end),
-	Text.new("Now you can fix it.",Rachel),
-	Text.new("I'm counting on you.",Rachel),
+	Text.new("Maintenant tu peux le réparer.",Rachel),
+	Text.new("Je t'attends.",Rachel),
 },1)
 
 World.Dialogue[French][32] = Dialogue.new({
-	Text.new("Hi Var!",Rachel),
-	Text.new("Hey",Varisa),
-	Text.new("You think you could stop by my house?",Rachel),
-	Text.new("Yeah, I could stop by.",Varisa),
-	Text.new("Great.",Rachel),
-	Text.new("I need you to ask my mom for the toolbox.",Rachel),
-	Text.new("Wait, what do you need from it?",Varisa),
-	Text.new("A screwdriver?",Rachel),
-	Text.new("Look what our old math teacher gave me.",Varisa),
-	Text.new("No way!",Rachel),
-	Text.new("That's exactly what I need!",Rachel),
-	Text.new("Well, that works I guess.",Varisa),
-	Text.new("Here, take my guitar.",Rachel,nil,function()
+	Text.new("Salut Var!",Rachel),
+	Text.new("Salut",Varisa),
+	Text.new("Tu penses que tu peux visiter ma maison?",Rachel),
+	Text.new("Ouais, Je peux visiter.",Varisa),
+	Text.new("Super.",Rachel),
+	Text.new("J'ai besoin que tu demandes la boîte à outils à ma mère.",Rachel),
+	Text.new("Attende, quoi tu as besoin des il?",Varisa),
+	Text.new("Un tournevis?",Rachel),
+	Text.new("Voir quoi notre vieille professeur de math m'a donner.",Varisa),
+	Text.new("Mais non!",Rachel),
+	Text.new("C'est exactement quel j'ai besoins!",Rachel),
+	Text.new("Eh bien, ça marche je suppose.",Varisa),
+	Text.new("Voilà, prends ma guitare.",Rachel,nil,function()
 		Story.set("Rachel_Guitar_1",true)
 	end),
-	Text.new("Now you can fix it.",Rachel),
-	Text.new("I'm counting on you.",Rachel),
+	Text.new("Maintenant tu peux le réparer.",Rachel),
+	Text.new("Je t'attends.",Rachel),
 },1)
