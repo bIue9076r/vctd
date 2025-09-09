26bec78162db0cc9a1ab593f0ca075b94199e508
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-09-08 19:58:40 -0500

    Dialogue changes

--- scenes/mollie_1.lua
+++ scenes/mollie_1.lua
@@ -1,28 +1,71 @@
 Play.Scenes[5] = Scene.new(World.Map[1])
 Play.Scenes[5].tickGoal = math.floor(9223372036854775807 / 10000000000000)--800
 Play.Scenes[5].Next = WORLD
-Play.Scenes[5].Actors[1] = Actor.new(Karly,0,12)
-Play.Scenes[5].Actors[2] = Actor.new(Mailbox,11.0,8.4)
+Play.Scenes[5].Hour = Evening
+
+Play.Scenes[5].Actors[1] = Actor.new(Mailbox,11.0,8.4)
+Play.Scenes[5].Actors[2] = Actor.new(Mollie,9.9,8.2)
+Play.Scenes[5].Actors[3] = Actor.new(Ella,16.6,9.6)
+Play.Scenes[5].Actors[4] = Actor.new(Sean,18.3,9.6)
 
 Play.Scenes[5].transition = function(self,t)
-	if(t < 200) then
+	if(t < 150) then
 		if t == 1 then
 			self.IsTalking = true
 			self.dtbl = {
-				s = String.get(4),
-				n = Karly,
+				s = String.get(14),
+				n = Ella,
 				v = "Normal",
 			}
 		end
-		self.Actors[1].x = Scene.lerp(-1,22,t/200)
-		if t == 199 then
-			self.Actors[1].x = 25
-		end
 	elseif(t < 300) then
-		self.Hour = Scene.lerp(0,7.5,(t - 200)/100)
-		self.Min = math.floor(60 * (self.Hour - math.floor(self.Hour)))
-		self.Hour = math.floor(self.Hour)
-	elseif(t < 800) then
+		if t == 151 then
+			self.IsTalking = true
+			self.dtbl = {
+				s = String.get(15),
+				n = Ella,
+				v = "Normal",
+			}
+		end
+	elseif(t < 450) then
+		if t == 301 then
+			self.IsTalking = true
+			self.dtbl = {
+				s = String.get(16),
+				n = Sean,
+				v = "Normal",
+			}
+		end
+	elseif(t < 600) then
+		if t == 451 then
+			self.IsTalking = true
+			self.dtbl = {
+				s = String.get(17),
+				n = Ella,
+				v = "Normal",
+			}
+		end
+	elseif(t < 750) then
+		if t == 601 then
+			self.IsTalking = true
+			self.dtbl = {
+				s = String.get(18),
+				n = Ella,
+				v = "Normal",
+			}
+		end
+	elseif(t < 900) then
+		if t == 751 then
+			self.IsTalking = true
+			self.dtbl = {
+				s = String.get(19),
+				n = Sean,
+				v = "Normal",
+			}
+		end
+	elseif(t < 1050) then
+		
+	elseif(t < self.tickGoal) then
 		
 	else
 		Voices[self.dtbl.v]:pause()
