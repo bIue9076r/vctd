7374d14f48227dd9aa38bb0430649ac0b659bdb3
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2026-05-03 23:15:54 -0500

    Update ticker.lua

--- Modules/ticker.lua
+++ Modules/ticker.lua
@@ -1,4 +1,4 @@
--- Ticker
+-- Ticker (Discrete)
 
 ticker = {}
 ticker.ticker = {
@@ -44,4 +44,53 @@ end
 
 function ticker:inc()
 	self.ticker.i = self.ticker.i + 1
+end
+
+
+-- Ticker (Continuous)
+
+tickerc = {}
+tickerc.ticker = {
+	i = 0,
+}
+tickerc.flags = {}
+
+function tickerc.new()
+	tbl = {
+		ticker = {
+			i = 0,
+		},
+		flags = {},
+	}
+	
+	mt = {
+		__index = tickerc,
+		__call = tickerc.inc,
+	}
+	
+	return setmetatable(tbl,mt)
+end
+
+function tickerc:flagSet(i,v)
+	self.flags[i] = v
+end
+
+function tickerc:flagGet(i)
+	return self.flags[i]
+end
+
+function tickerc:reset()
+	self.ticker.i = 0
+end
+
+function tickerc:set(v)
+	self.ticker.i = v
+end
+
+function tickerc:get()
+	return self.ticker.i
+end
+
+function tickerc:inc(dt)
+	self.ticker.i = self.ticker.i + dt
 end
