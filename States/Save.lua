08628431a491c20b6d75ecaa840a9c112679f37f
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2026-06-27 14:10:51 -0500

    save menus

--- States/Save.lua
+++ States/Save.lua
@@ -133,148 +133,181 @@ function Game_Delete(file)
 	file:Delete()
 end
 
+Save_Mode = 1
+Save_Modes = {
+	[1] = {
+		Draw = function()
+			Savesong:setVolume(Game_MusicVolume)
+			Savesong:play()
+			
+			-- draw background
+			drawBackgound()
+			
+			local img = image.getImage("Save")
+			
+			love.graphics.draw(
+				img,0,0,0,
+				SCREEN_X/img:getWidth(),
+				SCREEN_Y/img:getHeight()
+			)
+			
+			love.graphics.print(
+				{{0,0,0},String.get(22)},
+				((3*SCREEN_X)/5),(SCREEN_Y/4),
+				0,SCREEN_X/600,SCREEN_Y/400
+			)
+			
+			love.graphics.print(
+				{{0,0,0},String.get(23)..(ConfVer or CVERSION)},
+				((3*SCREEN_X)/5),((5*SCREEN_Y)/16),
+				0,SCREEN_X/600,SCREEN_Y/400
+			)
+			
+				love.graphics.print(
+				{{0,0,0},string.format(String.get(24),ConfSeed or Seed)},
+				((3*SCREEN_X)/5),((3*SCREEN_Y)/8),
+				0,SCREEN_X/600,SCREEN_Y/400
+			)
+			
+			love.graphics.print(
+				{{0,0,0},String.get(25)..tostring(ConfLang or SaveLang[Language])},
+				((3*SCREEN_X)/5),((7*SCREEN_Y)/16),
+				0,SCREEN_X/600,SCREEN_Y/400
+			)
+			
+			love.graphics.print(
+				{{0,0,0},String.get(26)..tostring(ConfDay or GameDay)},
+				((3*SCREEN_X)/5),(SCREEN_Y/2),
+				0,SCREEN_X/600,SCREEN_Y/400
+			)
+			
+			love.graphics.print(
+				{{1,0,0},SaveConfirm_1},
+				((3*SCREEN_X)/5),((9*SCREEN_Y)/16),
+				0,SCREEN_X/600,SCREEN_Y/400
+			)
+			
+			love.graphics.print(
+				{{1,0,0},SaveConfirm_2},
+				((3*SCREEN_X)/5),((5*SCREEN_Y)/8),
+				0,SCREEN_X/600,SCREEN_Y/400
+			)
+			
+			love.graphics.print(
+				{{0,0,0}," "},
+				((3*SCREEN_X)/5),((11*SCREEN_Y)/16),
+				0,SCREEN_X/600,SCREEN_Y/400
+			)
+			
+			love.graphics.print(
+				{{0,0,0},String.get(27)},
+				((3*SCREEN_X)/5),((3*SCREEN_Y)/4),
+				0,SCREEN_X/600,SCREEN_Y/400
+			)
+			
+			love.graphics.print(
+				{{0,0,0},String.get(28)},
+				((3*SCREEN_X)/5),((13*SCREEN_Y)/16),
+				0,SCREEN_X/600,SCREEN_Y/400
+			)
+		end,
+
+		Update = function(dt)
+			
+		end,
+
+		Keypressed = function(key)
+			if key == "s" then
+				if SaveCon then
+					SaveConfirm_1 = ""
+					SaveConfirm_2 = ""
+					SaveCon = false
+					Game_Save(savefile)
+				else
+					SaveConfirm_1 = String.get(29)
+					SaveConfirm_2 = String.get(30)
+					Game_clearData()
+					SaveCon = true
+				end
+				LoadCon = false
+				DeleCon = false
+			elseif key == "l" then
+				if LoadCon then
+					SaveConfirm_1 = ""
+					SaveConfirm_2 = ""
+					LoadCon = false
+					Game_Load(savefile)
+				else
+					SaveConfirm_1 = String.get(29)
+					SaveConfirm_2 = String.get(31)
+					Game_getData(savefile)
+					LoadCon = true
+				end
+				SaveCon = false
+				DeleCon = false
+			elseif key == "d" then
+				if DeleCon then
+					SaveConfirm_1 = ""
+					SaveConfirm_2 = ""
+					DeleCon = false
+					Game_Delete(savefile)
+				else
+					SaveConfirm_1 = String.get(29)
+					SaveConfirm_2 = String.get(32)
+					Game_getData(savefile)
+					DeleCon = true
+				end
+				LoadCon = false
+				SaveCon = false
+			elseif key == "return" or key == "escape" or key == "backspace" then
+				Game_clearData()
+				Savesong:stop()
+				SaveConfirm_1 = ""
+				SaveConfirm_2 = ""
+				LoadCon = false
+				SaveCon = false
+				DeleCon = false
+				GameState = Save_LastState
+			else
+				Game_clearData()
+				SaveConfirm_1 = ""
+				SaveConfirm_2 = ""
+				LoadCon = false
+				SaveCon = false
+				DeleCon = false
+			end
+		end,
+
+		Mousepressed = function(x,y,button)
+			
+		end,
+	},
+}
+
 function Save_Draw()
-	Savesong:setVolume(Game_MusicVolume)
-	Savesong:play()
 	
-	-- draw background
-	drawBackgound()
-	
-	local img = image.getImage("Save")
-	
-	love.graphics.draw(
-		img,0,0,0,
-		SCREEN_X/img:getWidth(),
-		SCREEN_Y/img:getHeight()
-	)
-	
-	love.graphics.print(
-		{{0,0,0},String.get(22)},
-		((3*SCREEN_X)/5),(SCREEN_Y/4),
-		0,SCREEN_X/600,SCREEN_Y/400
-	)
-	
-	love.graphics.print(
-		{{0,0,0},String.get(23)..(ConfVer or CVERSION)},
-		((3*SCREEN_X)/5),((5*SCREEN_Y)/16),
-		0,SCREEN_X/600,SCREEN_Y/400
-	)
-	
-		love.graphics.print(
-		{{0,0,0},string.format(String.get(24),ConfSeed or Seed)},
-		((3*SCREEN_X)/5),((3*SCREEN_Y)/8),
-		0,SCREEN_X/600,SCREEN_Y/400
-	)
-	
-	love.graphics.print(
-		{{0,0,0},String.get(25)..tostring(ConfLang or SaveLang[Language])},
-		((3*SCREEN_X)/5),((7*SCREEN_Y)/16),
-		0,SCREEN_X/600,SCREEN_Y/400
-	)
-	
-	love.graphics.print(
-		{{0,0,0},String.get(26)..tostring(ConfDay or GameDay)},
-		((3*SCREEN_X)/5),(SCREEN_Y/2),
-		0,SCREEN_X/600,SCREEN_Y/400
-	)
-	
-	love.graphics.print(
-		{{1,0,0},SaveConfirm_1},
-		((3*SCREEN_X)/5),((9*SCREEN_Y)/16),
-		0,SCREEN_X/600,SCREEN_Y/400
-	)
-	
-	love.graphics.print(
-		{{1,0,0},SaveConfirm_2},
-		((3*SCREEN_X)/5),((5*SCREEN_Y)/8),
-		0,SCREEN_X/600,SCREEN_Y/400
-	)
-	
-	love.graphics.print(
-		{{0,0,0}," "},
-		((3*SCREEN_X)/5),((11*SCREEN_Y)/16),
-		0,SCREEN_X/600,SCREEN_Y/400
-	)
-	
-	love.graphics.print(
-		{{0,0,0},String.get(27)},
-		((3*SCREEN_X)/5),((3*SCREEN_Y)/4),
-		0,SCREEN_X/600,SCREEN_Y/400
-	)
-	
-	love.graphics.print(
-		{{0,0,0},String.get(28)},
-		((3*SCREEN_X)/5),((13*SCREEN_Y)/16),
-		0,SCREEN_X/600,SCREEN_Y/400
-	)
+	if Save_Modes[Save_Mode] and Save_Modes[Save_Mode].Draw then
+		Save_Modes[Save_Mode].Draw()
+	end
 end
 
 function Save_Update(dt)
 	
+	if Save_Modes[Save_Mode] and Save_Modes[Save_Mode].Update then
+		Save_Modes[Save_Mode].Update(dt)
+	end
 end
 
 function Save_Keypressed(key)
-	if key == "s" then
-		if SaveCon then
-			SaveConfirm_1 = ""
-			SaveConfirm_2 = ""
-			SaveCon = false
-			Game_Save(savefile)
-		else
-			SaveConfirm_1 = String.get(29)
-			SaveConfirm_2 = String.get(30)
-			Game_clearData()
-			SaveCon = true
-		end
-		LoadCon = false
-		DeleCon = false
-	elseif key == "l" then
-		if LoadCon then
-			SaveConfirm_1 = ""
-			SaveConfirm_2 = ""
-			LoadCon = false
-			Game_Load(savefile)
-		else
-			SaveConfirm_1 = String.get(29)
-			SaveConfirm_2 = String.get(31)
-			Game_getData(savefile)
-			LoadCon = true
-		end
-		SaveCon = false
-		DeleCon = false
-	elseif key == "d" then
-		if DeleCon then
-			SaveConfirm_1 = ""
-			SaveConfirm_2 = ""
-			DeleCon = false
-			Game_Delete(savefile)
-		else
-			SaveConfirm_1 = String.get(29)
-			SaveConfirm_2 = String.get(32)
-			Game_getData(savefile)
-			DeleCon = true
-		end
-		LoadCon = false
-		SaveCon = false
-	elseif key == "return" or key == "escape" or key == "backspace" then
-		Game_clearData()
-		Savesong:stop()
-		SaveConfirm_1 = ""
-		SaveConfirm_2 = ""
-		LoadCon = false
-		SaveCon = false
-		DeleCon = false
-		GameState = Save_LastState
-	else
-		Game_clearData()
-		SaveConfirm_1 = ""
-		SaveConfirm_2 = ""
-		LoadCon = false
-		SaveCon = false
-		DeleCon = false
+	
+	if Save_Modes[Save_Mode] and Save_Modes[Save_Mode].Keypressed then
+		Save_Modes[Save_Mode].Keypressed(key)
 	end
 end
 
 function Save_Mousepressed(x,y,button)
 	
+	if Save_Modes[Save_Mode] and Save_Modes[Save_Mode].Mousepressed then
+		Save_Modes[Save_Mode].Mousepressed(x,y,button)
+	end
 end
