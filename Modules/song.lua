d8232243a313b0b5a52caca013b5efbc4a6e23f1
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2026-07-28 23:39:44 -0500

    shuffle

--- Modules/song.lua
+++ Modules/song.lua
@@ -2,12 +2,14 @@ Song = {}
 Song.moodMin = 0 -- Darkest Mood the Song Will Play
 Song.moodMax = 0 -- Brightest Mood the Song Will Play
 Song.Name = ""
+Song.count = 0
 
 function Song.new(n,mn,mx)
 	local tbl = {
 		Name = n or "",
 		moodMin = mn or 0,
 		moodMax = mx or 0,
+		count = 0,
 	}
 	
 	local mt = {
@@ -24,16 +26,37 @@ end
 
 function SongListSelect(List)
 	local okTbl = {}
+	local cnTbl = {}
 	
 	for i,v in pairs(List) do
 		if v:canPlay() then
 			table.insert(okTbl,v.Name)
+			table.insert(cnTbl,v.count)
 		end
 	end
-
+	
 	if #okTbl == 0 then
 		return nil
 	end
 	
-	return okTbl[math.random(1,#okTbl)]
+	local n = math.random(1,#okTbl)
+	for _ = 1,5 do -- 5 tries
+		local m = false
+		
+		for i = 1,#cnTbl do
+			if not (i == n) then
+				if (cnTbl[n] - cnTbl[i]) >= 2 then
+					m = true
+				end
+			end
+		end
+		
+		if m then
+			n = math.random(1,#okTbl)
+		else
+			break
+		end
+	end
+	
+	return okTbl[n]
 end
